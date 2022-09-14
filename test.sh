6be25edaa1c41d1828059707c32c6d96189df23d
doubleopinter <doubleopinter@users.noreply.github.com>
2022-09-14 16:37:49 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-#this is a test
+#!/bin/sh
+
+printf "network:\n\trenderer: networkd\n\tversion: 2\n\tethernets:\n\t\tenp1s0:\n\t\t\tdhcp4: true\n\t\tenp2s0:\n\t\t\tdhcp4: true" > /etc/netplan/rescue.yaml
+netplan generate
+netplan apply
+
+ip a
