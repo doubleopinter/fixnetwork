4ddb1052fb3528b8c8ed12dd1ed06387406726a9
doubleopinter <doubleopinter@users.noreply.github.com>
2022-09-14 16:50:20 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf "network:\n\trenderer: networkd\n\tversion: 2\n\tethernets:\n\t\tenp1s0:\n\t\t\tdhcp4: true\n\t\tenp2s0:\n\t\t\tdhcp4: true" > /etc/netplan/rescue.yaml
+printf "network:\n  renderer: networkd\n  version: 2\n  ethernets:\n    enp1s0:\n      dhcp4: true\n    enp2s0:\n      dhcp4: true" > /etc/netplan/rescue.yaml
 netplan generate
 netplan apply
 
